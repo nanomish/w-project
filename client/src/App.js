5758748639c2ab3d556fa2eb1581791729583d2b
nanomish <michael.kushniriov@gmail.com>
2020-03-19 23:14:16 +0200

    search

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,16 @@ import ContactElement from './components/Contact/ContactElement';
 
 function App() {
   const [contacts, setContacts] = useState(null);
+  const [search, setSearch] = useState(null);
+  const [currentPage, setCurrentPage] = useState(null);
+
+  const PAGE_SIZE = 5;
 
   useEffect(() => {
     if(!contacts) {
       getContacts();
     }
+    setCurrentPage(1);
   });
 
   const getContacts = async () => {
@@ -19,19 +24,49 @@ function App() {
     setContacts(res);
   };
 
+  const onPrev = () => {
+
+  }
+
+  const onNext = () => {
+
+  }
+
   return (
     <div className="contacts-main-view">
       <InputContact getContacts={getContacts}/>
+
+      <div className="search-contacts">
+        <input type="text" placeholder="search"
+               onChange={e => setSearch(e.target.value)}
+               className="search-contacts-i"
+        />
+      </div>
       <div className="list-item">
         {(contacts && contacts.length > 0) ? (
-          contacts.map(contact => {
-            return <ContactElement key={contact._id} getContacts={getContacts} contact={contact} />
-          })
+          contacts
+            .filter(contact => {
+              const searchRegexp = new RegExp(search, 'i');
+              return search && (contact.name.match(searchRegexp) || contact.phone.match(searchRegexp)) || !search
+            })
+            .map(contact => {
+              return <ContactElement key={contact._id} getContacts={getContacts} contact={contact} />
+            })
         ) : (
           <div className="no-contacts">
             <span className="no-contacts-msg">You have no contacts</span>
           </div>
         )}
+      </div>
+      <div className="contacts-pagination">
+        <input type="submit"
+               className="pointed page-button"
+               value="<<"
+               onClick={() => onPrev()}/>
+        <input type="submit"
+               className="pointed page-button"
+               value=">>"
+               onClick={() => onNext()}/>
       </div>
     </div>
   );
